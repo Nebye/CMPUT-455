3464b280db85a936904c60d133544b7136e9622e
Nebye <nebyeb@hotmail.com>
2021-02-28 16:10:33 -0700

    final change

--- Assignment_02 - Revengence/evaluation.py
+++ Assignment_02 - Revengence/evaluation.py
@@ -11,34 +11,33 @@ def calc_score(counts, color):
         opp_count, my_count, open_count = counts
     if (my_count >= 1 and opp_count >= 1):
         return 0
-
     return value_coor[my_count] - value_coor[opp_count]
 
 
 def get_counts(board, five_line):
-    b_count = 0
     w_count = 0
-    e_count = 0
+    b_count = 0
+    e_count = 0    
 
     for p in five_line:
         stone = board.board[p]
-        if stone == BLACK:
-            b_count += 1
-        elif stone == WHITE:
-            w_count += 1
+        if (stone == BLACK):
+            b_count = b_count + 1
+        elif (stone == WHITE):
+            w_count = w_count + 1
         else:
-            e_count += 1
+            e_count = e_count + 1
 
     return b_count, w_count, e_count
 
 
 def evaluate(board, color):
-    score = 0
+    sPoints = 0
     lines = board.rows + board.cols + board.diags
 
     for line in lines:
         for i in range(len(line) - 5):
             counts = get_counts(board, line[i:i+5])
-            score += calc_score(counts, color)
+            sPoints += calc_score(counts, color)
 
-    return score
+    return sPoints
