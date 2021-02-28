fa15c28c3632859b4ef1b58dbd5510355152b83d
Nebye <nebyeb@hotmail.com>
2021-02-28 15:30:22 -0700

    eval

--- Assignment_02 - Revengence/evaluation.py
+++ Assignment_02 - Revengence/evaluation.py
@@ -15,27 +15,26 @@ def calc_score(counts, color):
 
 
 def get_counts(board, five_line):
-    b_count = 0
-    w_count = 0
-    e_count = 0
+    b_count, w_count, e_count = 0
 
-    for p in five_line:
-        stone = board.board[p]
+    for i in five_line:
+        stone = board.board[i]
         if stone == BLACK:
-            b_count += 1
+            b_count = b_count + 1
         elif stone == WHITE:
-            w_count += 1
+            w_count = w_count + 1
         else:
-            e_count += 1
+            e_count = e_count + 1
     return b_count, w_count, e_count
 
 
 def evaluate(board, color):
-    lines = board.rows + board.cols + board.diags
-    score = 0
+    lines = board.rows + board.cols
+    lines = lines + board.diags
+    points = 0
 
     for line in lines:
-        for i in range(len(line) - 5):
-            counts = get_counts(board, line[i:i+5])
-            score += calc_score(counts, color)
-    return score
+        for x in range(len(line) - 5):
+            counts = get_counts(board, line[x:x+5])
+            points = points + calc_score(counts, color)
+    return points
