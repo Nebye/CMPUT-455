25a739004c552e05f7bf1be55b4622cc6ecfc7df
Nebye <nebyeb@hotmail.com>
2021-02-28 15:50:40 -0700

    eval2.0

--- Assignment_02 - Revengence/evaluation.py
+++ Assignment_02 - Revengence/evaluation.py
@@ -1,39 +1,43 @@
-from board_util import BLACK, WHITE, EMPTY
-import collections
-
-POINTS = [0, 1, 2, 5, 15, 1000000]
+SCORE_MAP = [0, 1, 2, 5, 15, 1000000]
 
 
 def calc_score(counts, color):
-    if (color == BLACK):
+    if color == BLACK:
         my_count, opp_count, open_count = counts
     else:
         opp_count, my_count, open_count = counts
-    if (opp_count >= 1 and my_count >= 1):
+
+    # Is blocked
+    if my_count >= 1 and opp_count >= 1:
         return 0
-    return POINTS[my_count] - POINTS[opp_count]
+
+    return SCORE_MAP[my_count] - SCORE_MAP[opp_count]
 
 
 def get_counts(board, five_line):
-    b_count, w_count, e_count = 0
+    b_count = 0
+    w_count = 0
+    e_count = 0
 
-    for i in five_line:
-        stone = board.board[i]
+    for p in five_line:
+        stone = board.board[p]
         if stone == BLACK:
-            b_count = b_count + 1
+            b_count += 1
         elif stone == WHITE:
-            w_count = w_count + 1
+            w_count += 1
         else:
-            e_count = e_count + 1
+            e_count += 1
+
     return b_count, w_count, e_count
 
 
 def evaluate(board, color):
-    lines = board.rows + board.cols + board.diags
     score = 0
+    lines = board.rows + board.cols + board.diags
 
     for line in lines:
-        for x in range(len(line) - 5):
-            counts = get_counts(board, line[x:x+5])
-            score = score + calc_score(counts, color)
+        for i in range(len(line) - 5):
+            counts = get_counts(board, line[i:i+5])
+            score += calc_score(counts, color)
+
     return score
