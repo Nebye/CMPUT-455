d34ac4e5d8f13b6bbc3d7e8fb1cdaf026939a8ac
Nebye <nebyeb@hotmail.com>
2021-02-28 21:59:38 -0700

    one more time

---
+++ Assignment_02 - Re2/TranspositionTable.py
@@ -0,0 +1,16 @@
+import random
+
+
+class TranspositionTable:
+    def __init__(self):
+        self.table = {}
+
+    def store(self, code, result):
+        # result is a tuple: (score, best_move)
+        self.table[code] = result
+
+    def lookup(self, code):
+        return self.table.get(code)
+
+    def returnTable(self):
+        return self.table
